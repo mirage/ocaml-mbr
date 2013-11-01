53dffb09e2c14050aefbbfcac9bafbaa66604211
David Scott <dave.scott@eu.citrix.com>
2013-11-01 15:35:31 +0000

    Parse "modern standard" MBR via cstruct
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/mbr.ml
+++ lib/mbr.ml
@@ -12,3 +12,23 @@
  * GNU Lesser General Public License for more details.
  *)
 
+(* "modern standard" MBR from wikipedia: *)
+cstruct mbr {
+  uint8_t bootstrap_code[218];
+  uint8_t _zeroes_1[2];
+  uint8_t original_physical_drive;
+  uint8_t seconds;
+  uint8_t minutes;
+  uint8_t hours;
+  uint8_t bootstrap_code2[216];
+  uint32_t disk_signature;
+  uint8_t _zeroes_2[2];
+  uint8_t partition1[16];
+  uint8_t partition2[16];
+  uint8_t partition3[16];
+  uint8_t partition4[16];
+  uint8_t signature[2]  (* 0x55; 0xaa *)
+} as little_endian
+
+let _ = assert(sizeof_mbr = 512)
+
