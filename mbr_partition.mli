4a0f1c2ebf0bbb0aa29e47dde6437340cdfb1941
David Scott <dave.scott@citrix.com>
2014-08-18 13:33:23 +0000

    Add missing file: mbr_partition.mli
    Signed-off-by: David Scott <dave.scott@citrix.com>

---
+++ mbr_partition.mli
@@ -0,0 +1,23 @@
+(*
+ * Copyright (C) 2011-2013 Citrix Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+module Make(B: V1_LWT.BLOCK) : sig
+  type id = {
+    b: B.t;
+    start_sector: int64;
+    length_sectors: int64;
+  }
+
+  include V1_LWT.BLOCK
+    with type id := id
+end
