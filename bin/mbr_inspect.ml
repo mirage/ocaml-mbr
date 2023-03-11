cf0992b767f821b350fe9b4d56b2fa787f08cdce
Pixie Dust <playersrebirth@gmail.com>
2023-03-11 10:33:06 +0100

    modify script to read MBR content from arguments passed with the command

--- bin/mbr_inspect.ml
+++ bin/mbr_inspect.ml
@@ -32,5 +32,14 @@ let read_mbr mbr =
       exit 1
  
 let () =
-  let mbr = read_mbr "test.img" in
-  print_mbr_fields mbr
+    let command = Sys.argv.(0) in
+    if Array.length Sys.argv < 2 then begin
+      Printf.printf "Inspect MBR Headers: \n Usage %s <file1> [<file2> ...]\n You must pass in at least one MBR header\n" command; 
+      exit 1
+    end else
+
+    let mbr_files = Array.of_list(Array.to_list(Array.sub Sys.argv 1 (Array.length Sys.argv -1))) in
+    Array.iter (fun mbr_file ->
+      let mbr = read_mbr mbr_file in
+      print_mbr_fields mbr
+    ) mbr_files
