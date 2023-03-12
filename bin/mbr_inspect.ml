598c51e0644cdc382c5fb474242dcec0bce1e1f3
Pixie Dust <playersrebirth@gmail.com>
2023-03-12 11:05:34 +0100

    ocamlformat whitespace fixes

--- bin/mbr_inspect.ml
+++ bin/mbr_inspect.ml
@@ -39,24 +39,23 @@ let read_mbr mbr =
   | Error msg ->
       Printf.printf "Failed to read MBR from %s: %s\n" mbr msg;
       exit 1
- 
-      let () =
-        let command = Sys.argv.(0) in
-        if Array.length Sys.argv < 2 then (
-          Printf.printf
-            "Inspect MBR Headers: \n\
-            \ Usage %s <file1> [<file2> ...]\n\
-            \ You must pass in at least one MBR header\n"
-            command;
-          exit 1)
-        else
-          let mbr_files =
-            Array.of_list
-              (Array.to_list (Array.sub Sys.argv 1 (Array.length Sys.argv - 1)))
-          in
-          Array.iter
-            (fun mbr_file ->
-              let mbr = read_mbr mbr_file in
-              print_mbr_fields mbr)
-            mbr_files 
-            
+
+let () =
+  let command = Sys.argv.(0) in
+  if Array.length Sys.argv < 2 then (
+    Printf.printf
+      "Inspect MBR Headers: \n\
+      \ Usage %s <file1> [<file2> ...]\n\
+      \ You must pass in at least one MBR header\n"
+      command;
+    exit 1)
+  else
+    let mbr_files =
+      Array.of_list
+        (Array.to_list (Array.sub Sys.argv 1 (Array.length Sys.argv - 1)))
+    in
+    Array.iter
+      (fun mbr_file ->
+        let mbr = read_mbr mbr_file in
+        print_mbr_fields mbr)
+      mbr_files
