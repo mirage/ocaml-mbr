a8410e466839a7ffd5c8630eae6fd4a51d59eed4
Reynir Björnsson <reynir@reynir.dk>
2023-04-04 15:16:08 +0200

    write_partition: Simplify copying, check sizes
    Reading is done until EOF is hit. While writing an error is raised if
    more is read than what the partition allows for. If the input is a file
    the size is checked early against the size reported by `Unix.stat`.

--- bin/write_partition.ml
+++ bin/write_partition.ml
@@ -16,6 +16,7 @@ let get_partition_info mbr partition_num =
   List.nth mbr.Mbr.partitions (partition_num - 1)
 
 let calculate_partition_info partition =
+  (* FIXME: Use Int32.unsigned_to_int *)
   let start_sector =
     Int32.to_int partition.Mbr.Partition.first_absolute_sector_lba
   in
@@ -23,86 +24,53 @@ let calculate_partition_info partition =
   let sector_size = 512 in
   (start_sector, num_sectors, sector_size)
 
-let read_multiline () =
-  let rec read_lines acc =
-    try
-      let line = input_line stdin in
-      if String.length line > 0 && line.[0] = '>' then
-        String.concat "\n" (List.rev acc)
-      else read_lines (line :: acc)
-    with End_of_file -> String.concat "\n" (List.rev acc)
+let copy ic oc max_bytes =
+  let buf_len = 4096 in
+  let buf = Bytes.create buf_len in
+  let rec loop i =
+    let len = input ic buf 0 buf_len in
+    if len > 0 then
+      let len' = min len (max_bytes - i) in
+      output oc buf 0 len';
+      if i + len > max_bytes then
+        failwith "Trying to write more than can fit in partition";
+      loop (i + len')
+    else
+      ()
   in
-  read_lines []
-
-let read_file file_path chunk_size pos =
-  Printf.printf "\nRead position in data file: %d\n" pos;
-  let ic = open_in_bin file_path in
-  try
-    seek_in ic pos;
-    let bytes = Bytes.create chunk_size in
-    let bytes_read = input ic bytes 0 chunk_size in
-    close_in ic;
-    Bytes.sub_string bytes 0 bytes_read
-  with exn ->
-    close_in ic;
-    raise exn
-
-let write_partition_data_internal fd start_sector write_sector output_buffer =
-  Printf.printf "Position written on disk: %d\n" write_sector;
-  let sector_size = 512 in
-  let pos = (start_sector * sector_size) + write_sector in
-  let () = seek_out fd pos in
-  let () = output_bytes fd output_buffer in
-  ()
+  loop 0
 
 let write_to_partition mbr partition_number input_data =
   let partition = get_partition_info mbr partition_number in
   let start_sector, num_sectors, sector_size =
     calculate_partition_info partition
   in
-  let start_position = 0 in
-  let data_chunk_size = 1024 in
-  let buffer, data_size =
+  if start_sector = 0 then
+    Printf.ksprintf failwith "Writing to partition %d would overwrite the MBR header" partition_number;
+  let ic, data_size =
     match input_data with
     | None ->
-        let input_string = Bytes.of_string (read_multiline ()) in
-        (input_string, Bytes.length input_string)
+        (stdin, None)
     | Some file_path ->
         let file_info = Unix.stat file_path in
         let data_size = file_info.st_size in
-        let file_contents =
-          read_file file_path data_chunk_size start_position |> Bytes.of_string
-        in
-        (file_contents, data_size)
+        let ic = open_in_bin file_path in
+        (ic, Some data_size)
   in
   let partition_size = num_sectors * sector_size in
-  Printf.printf "Total input size: %d\n" data_size;
-  Printf.printf "Chunk size: %d\n" data_chunk_size;
+  Option.iter (fun data_size ->
+      Printf.printf "Total input size: %d\n" data_size)
+    data_size;
   Printf.printf "Total Partition size: %d\n" partition_size;
-  if data_size > partition_size then failwith "Input is too large for partition"
-  else (
-    Printf.printf "\nBegin writing to partition:- \n";
-    let fd = open_out_gen [ Open_wronly; Open_binary ] 0o644 mbr in
-    let rec write_chunks start_pos =
-      if start_pos >= data_size then ()
-      else
-        let chunk_size = min data_chunk_size (data_size - start_pos) in
-        if chunk_size <= 0 then ()
-        else
-          let chunk =
-            match input_data with
-            | None ->
-                let chunk = Bytes.create chunk_size in
-                Bytes.blit buffer start_pos chunk 0 chunk_size;
-                chunk
-            | Some file_path ->
-                read_file file_path chunk_size start_pos |> Bytes.of_string
-          in
-          write_partition_data_internal fd start_sector start_pos chunk;
-          write_chunks (start_pos + chunk_size)
-    in
-    write_chunks 0;
-    close_out fd)
+  Option.iter (fun data_size ->
+      if data_size > partition_size then failwith "Input is too large for partition")
+    data_size;
+  Printf.printf "\nBegin writing to partition:- \n";
+  let oc = open_out_gen [ Open_wronly; Open_binary ] 0o644 mbr in
+  seek_out oc (start_sector * sector_size);
+  copy ic oc partition_size;
+  close_out_noerr oc;
+  close_in_noerr ic
 
 let mbr =
   let doc = "The disk image containing the partition" in
@@ -114,7 +82,7 @@ let partition_number =
 
 let input_data =
   let doc = "The data to write to the partition." in
-  Arg.(value & opt (some string) None & info [ "d"; "data" ] ~doc)
+  Arg.(value & opt (some string) None & info [ "d"; "data" ] ~doc ~docv:"FILE")
 
 let cmd =
   let doc = "Write data into a partition" in
