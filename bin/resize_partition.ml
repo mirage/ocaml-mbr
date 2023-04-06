430bb02d0ead9007dfe814204bf110e4010da4cb
Pixie Dust <playersrebirth@gmail.com>
2023-04-06 12:59:28 +0100

    read mbr from disk and grab information about partition

--- bin/resize_partition.ml
+++ bin/resize_partition.ml
@@ -1,7 +1,35 @@
 open Cmdliner
 
+let read_mbr mbr =
+  let ic = open_in_bin mbr in
+  let buf = Bytes.create Mbr.sizeof in
+  let () = really_input ic buf 0 Mbr.sizeof in
+  close_in ic;
+  match Mbr.unmarshal (Cstruct.of_bytes buf) with
+  | Ok mbr -> (mbr, Mbr.sizeof)
+  | Error msg ->
+      Printf.printf "Failed to read MBR from %s: %s\n" mbr msg;
+      exit 1
+
+let get_partition_info mbr partition_num =
+  let mbr = read_mbr mbr |> fst in
+  List.nth mbr.Mbr.partitions (partition_num - 1)
+
+let calculate_partition_info partition =
+  (* FIXME: Use Int32.unsigned_to_int *)
+  let start_sector =
+    Int32.to_int partition.Mbr.Partition.first_absolute_sector_lba
+  in
+  let num_sectors = Int32.to_int partition.Mbr.Partition.sectors in
+  let sector_size = 512 in
+  (start_sector, num_sectors, sector_size)
+
 (*to be implemented*)
-let resize_partition _disk _partition_number _new_size =
+let resize_partition disk partition_number _new_size =
+  let partition = get_partition_info disk partition_number in
+  let _start_sector, _num_sectors, _sector_size =
+    calculate_partition_info partition
+  in
   ()
 
 let mbr =
