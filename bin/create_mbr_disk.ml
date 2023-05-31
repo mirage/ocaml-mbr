a22f307b1b795b2f6ae924542eb93597883b7bb7
PizieDust <playersrebirth@gmail.com>
2023-05-31 20:25:31 +0100

    fix linting

--- bin/create_mbr_disk.ml
+++ bin/create_mbr_disk.ml
@@ -7,41 +7,37 @@ let create_mbr_disk destination partition_files =
 
   let partition_sizes =
     List.map (fun file -> (Unix.stat file).Unix.st_size) partition_files
-  in    
+  in
 
-  let partition_size = List.fold_left (+) 0 partition_sizes in
+  let partition_size = List.fold_left ( + ) 0 partition_sizes in
   let mbr_size = Mbr.sizeof in
   let total_size = partition_size + mbr_size in
 
-  Printf.printf "Total disk size: %d bytes\n"
-      total_size;
+  Printf.printf "Total disk size: %d bytes\n" total_size;
 
   let partitions =
-    List.mapi (fun i size ->
-      Printf.printf "Creating partition: %d" (i+1);
-      let start_sector = (i + 1) * sector_size in
-      let num_sectors = (size + sector_size - 1) / sector_size in
-      match
-        Mbr.Partition.make ~active:false ~partition_type:6 (Int32.of_int start_sector) (Int32.of_int num_sectors)
-      with
-      | Ok partition ->
-          Printf.printf " - OK\n";
-          partition
-      | Error msg -> failwith msg
-    ) partition_sizes
+    List.mapi
+      (fun i size ->
+        Printf.printf "Creating partition: %d" (i + 1);
+        let start_sector = (i + 1) * sector_size in
+        let num_sectors = (size + sector_size - 1) / sector_size in
+        match
+          Mbr.Partition.make ~active:false ~partition_type:1
+            (Int32.of_int start_sector)
+            (Int32.of_int num_sectors)
+        with
+        | Ok partition ->
+            Printf.printf " - OK\n";
+            partition
+        | Error msg -> failwith msg)
+      partition_sizes
   in
-  
   (* Mbr.make smart constructor checks for partition overlap, more than 1 active partitions and too many partitions *)
   let mbr =
     match Mbr.make partitions with
     | Ok mbr -> mbr
     | Error msg -> failwith ("Failed to create MBR: " ^ msg)
-  in  
-
-  let oc = open_out_gen [ Open_wronly; Open_binary; Open_creat; Open_trunc ] 0o644 destination in
-  seek_out oc (total_size - 1);  (* Seek to the last byte of the file *)
-  output_byte oc 0;  (* Write a single byte to extend the file size *)
-  close_out oc;
+  in
 
   let oc = open_out_bin destination in
   let mbr_buffer = Cstruct.create Mbr.sizeof in
@@ -52,7 +48,10 @@ let create_mbr_disk destination partition_files =
 
 let destination =
   let doc = "Output file for the MBR formatted disk image" in
-  Arg.(required & opt (some string) None & info ["d"; "destination"] ~docv:"destination" ~doc)
+  Arg.(
+    required
+    & opt (some string) None
+    & info [ "d"; "destination" ] ~docv:"destination" ~doc)
 
 let partition_files =
   let doc = "Data files to be written to the partitions" in
